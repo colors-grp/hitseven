84032aa4e508c3946a55b1fac48f3d27b19d7140
heba-colors <heba@colors-grp.com>
2014-02-23 10:12:07 +0200

    Added grid view buy feature

--- Bonjour-Platform/platform/application/views/ajax/card_grid_view_ajax.php
+++ Bonjour-Platform/platform/application/views/ajax/card_grid_view_ajax.php
@@ -1,27 +1,36 @@
-</div> <!-- card List view end -->
+</div>
+<!-- card List view end -->
 <div id="grid_view">
-    <? foreach ($cards->result() as $card) {
+	<? foreach ($cards->result() as $card) {
        if (in_array($card->id, $user_cards)) { ?>
-            <div class="view_card">
-                <div class="grid-header">
-                    <span class="grid-card-name"> <?= $card->name ?></span>
-                    <span class="grid-card-point"> <?= $card->price ?> Points </span>
-                </div>
-                <a> <!-- on click should show card content -->
-                    <img src = "<?=base_url()?>h7-assets/images/categories/<?=$cat_name?>/cards/<?=$card->id?>/ui/grid_view.png" width="241" height="308" border="0">
-                </a>
-            </div>
-    <? } else { ?>
-            <div class="view_card">
-                <div class="grid-header">
-                    <span class="grid-card-name"> <?= $card->name?></span>
-                    <span class="grid-card-point"> <?= $card->price ?> Points </span>
-                </div>
-                <a>
-                    <div class="view_card_get">&nbsp;</div>
-                    <img src = "<?=base_url()?>h7-assets/images/categories/<?=$cat_name?>/cards/<?=$card->id?>/ui/grid_view.png"  width="241" height="308" border="0">
-                </a>
-            </div>
-    <? } 
+	<div class="view_card">
+		<div class="grid-header">
+			<span class="grid-card-name"> <?= $card->name ?>
+			</span> <span class="grid-card-point"> <?= $card->price ?> Points
+			</span>
+		</div>
+		<a href="javascript:void(0)"
+			onclick="show_card_content('<?=$card->category_id;?>','<?=$card->id?>','<?=$card->name?>','<?=$card->price?>','<?=$card->score?>')">
+			<img
+			src="<?=base_url()?>h7-assets/images/categories/<?=$cat_name?>/cards/<?=$card->id?>/ui/grid_view.png"
+			width="241" height="308" border="0">
+		</a>
+	</div>
+	<? } else { ?>
+	<div class="view_card">
+		<div class="grid-header">
+			<span class="grid-card-name"> <?= $card->name?>
+			</span> <span class="grid-card-point"> <?= $card->price ?> Points
+			</span>
+		</div>
+		<a href="javascript:void(0)"
+			onclick="show_card_content('<?=$card->category_id;?>','<?=$card->id?>','<?=$card->name?>','<?=$card->price?>','<?=$card->score?>')">
+			<div class="view_card_get">&nbsp;</div> <img
+			src="<?=base_url()?>h7-assets/images/categories/<?=$cat_name?>/cards/<?=$card->id?>/ui/grid_view.png"
+			width="241" height="308" border="0">
+		</a>
+	</div>
+	<? } 
  } ?>
-</div> <!-- card  Grid view end -->
+</div>
+<!-- card  Grid view end -->
