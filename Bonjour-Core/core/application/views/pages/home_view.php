32a8ba54499dab9480f1d07bc0e33e1c986afd17
Amr Hussein <amr@colors-grp.com>
2014-04-06 14:35:02 +0200

    Clean All ...

--- Bonjour-Core/core/application/views/pages/home_view.php
+++
@@ -1,3 +0,0 @@
-<?php echo anchor($this->facebook->getLoginURL($this->config->item('facebook_login_parameters')), 'Connect with Facebook', 'id=connect_button class=bambambam'); // Set the target URL for logging on to point to the H7 Core getLoginURL//echo anchor($this, 'Connect with Facebook', 'id=connect_button class=bambambam');?>
-
-<h3>Here is the Home page content ...</h3>
