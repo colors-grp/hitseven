a232b08e387e48146e7e904963971a52fbf98982
Amr Hussein <amr@colors-grp.com>
2014-04-17 10:56:36 +0200

    Config folder renamed as config2

---
+++ Bonjour-Core/core/application/config2/Facebook.php
@@ -0,0 +1,5 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+// $config['appId']  = 'APP_ID'; 
+// $config['secret'] = 'APP_SECRET';// Colors studios App
+$config['appId']   = '352621514881126';
+$config['secret']  = 'cd54e94fcc7509e7140def0b70fb4e59';
